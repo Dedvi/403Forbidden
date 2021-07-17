0fc014857a6960cfe06ca0e6359f4bce47e47af5
Janirudev <me@janiru.dev>
2021-07-17 23:39:26 +0400

    refactored translation to api endpoint

--- server/src/api/translate.js
+++ server/src/api/translate.js
@@ -1,17 +1,36 @@
+const express = require('express');
 const translate = require("translate");
+const Joi = require('joi');
+const {authenticateToken} = require('../middlewares');
+
+const router = express.Router();
 
 translate.engine = "libre";
 translate.url = "https://libretranslate.de/translate";
 
-async function translateString(string, fromLanguage, toLanguage){
-    try {
-        const translated = await translate(string, {from: fromLanguage, to: toLanguage});
-        console.log(translated);
-        return translated;
-    } catch (error) {
-        console.error(error);
-    }
-
-}
+// Create Validation Schema
+const transationSchema = Joi.object({
+    text: Joi.string().alphanum().required(),
+    from: Joi.string().alphanum().required(),
+    to: Joi.string().alphanum().required(),
+})
 
 // translateString('Hello', 'English', 'Hindi');
+
+router.get('/', authenticateToken, async (req, res, next) => {
+    const {text, from, to} = req.body
+    try {
+        await transationSchema.validateAsync({text, from, to});
+        const translated = await translate(text, {from, to});
+        res.json({
+            orginal: text,
+            translated,
+            from, 
+            to
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
