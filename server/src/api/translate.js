a67ba85b74ed4e2d6536400c5182ffaf5e991e3f
Janirudev <me@janiru.dev>
2021-07-18 09:38:09 +0400

    Defintions API completed/refactored and user get endpoint fixed

--- server/src/api/translate.js
+++ server/src/api/translate.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fetch = require('node-fetch');
 const translate = require("translate");
 const Joi = require('joi');
 const {authenticateToken} = require('../middlewares');
@@ -15,9 +16,13 @@ const transationSchema = Joi.object({
     to: Joi.string().alphanum().required(),
 })
 
+const dictionarySchema = Joi.object({
+    word: Joi.string().alphanum().required(),
+})
+
 // translateString('Hello', 'English', 'Hindi');
 
-router.get('/', authenticateToken, async (req, res, next) => {
+router.post('/', authenticateToken, async (req, res, next) => {
     const {text, from, to} = req.body
     try {
         await transationSchema.validateAsync({text, from, to});
@@ -28,6 +33,30 @@ router.get('/', authenticateToken, async (req, res, next) => {
             from, 
             to
         });
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/define', authenticateToken, async (req, res, next) => {
+    const { word } = req.body
+    try {
+        const lang = "en_US"
+        await dictionarySchema.validateAsync({ word });
+        let url = "https://api.dictionaryapi.dev/api/v2/entries/" + lang + "/" + word;
+
+        let settings = { method: "Get" };
+        //store phonetics and definitions (phonetics are in article language meaning is in native language)
+        const phonetics = [];
+
+        fetch(url, settings)
+            .then(res => res.json())
+            .then((json) => {
+                for (let i = 0; i < json[0].phonetics.length; i++) {
+                    phonetics.push([json[0].phonetics[i].text, json[0].phonetics[i].audio])
+                }
+                res.json({ orginal: word, phonetics, lang });
+            });
     } catch (error) {
         next(error);
     }
